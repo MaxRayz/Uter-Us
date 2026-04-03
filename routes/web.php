2ad305b7581a71e687922f9c49acc6de89c67b56
unknown <aliyaroispaculan@gmail.com>
2026-04-03 12:03:46 +0800

    fix the missing route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PeriodController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +13,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return redirect()->route('periods.index'); })->name('dashboard');
 
     Route::resource('periods', PeriodController::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
